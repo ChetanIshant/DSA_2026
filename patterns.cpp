31ce86a68a0158b5c01ba7b2ef49f8a48311c97a
Chetan Urf Ishant <ishunara25@gmail.com>
2026-05-27 23:38:28 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -11,3 +11,101 @@ int main(){
 		cout<<endl;
 	}
 }
+
+//2
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter the number of lines: ";
+	cin>>n;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<i;j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
+//3
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter the number of lines: ";
+	cin>>n;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<i;j++){
+			 cout<<j+1;
+		}
+		cout<<endl;
+	}
+}
+
+//4
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter the number of lines: ";
+	cin>>n;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+			 cout<<i+1;
+		}
+		cout<<endl;
+	}
+}
+
+//5
+#include<iostream>
+using namespace std;
+int main(){
+	int n, m;
+	cout<<"Enter the number of lines: ";
+	cin>>n;
+	m=n;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n-i;j++){
+			 cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
+//6
+#include<iostream>
+using namespace std;
+int main(){
+	int n, m;
+	cout<<"Enter the number of lines: ";
+	cin>>n;
+	m=n;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n-i;j++){
+			 cout<<j+1;
+		}
+		cout<<endl;
+	}
+}
+
+//7
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter the number of lines: ";
+	cin>>n;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n-i-1;j++){
+			 cout<<"  ";
+		}
+		for(int k=0;k<i;k++){
+			cout<<"* ";
+		}
+		for(int m=0;m<=i;m++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}

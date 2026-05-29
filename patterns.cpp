0b35d02ed74acd82cbb38fc0ce1b6e4df2219e76
Chetan Urf Ishant <ishunara25@gmail.com>
2026-05-29 21:18:21 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -1,5 +1,4 @@
 //1
-
 #include<iostream>
 using namespace std;
 int main(){
@@ -231,3 +230,30 @@ int main(){
 	}
 	
 }
+
+//12
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number of lines: ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<j+1;
+        }
+        for(int k=0;k<(((n*2)-2)-(i*2));k++)
+        {
+            cout<<" ";
+        }
+        for(int l=0;l<=i;l++)
+        {
+            cout<<((i+1)-l);
+        }
+        cout<<endl;
+    }
+}
+

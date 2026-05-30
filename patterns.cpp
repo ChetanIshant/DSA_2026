3709f5f02c56648b0fb96ffa5910d81a4c64868c
Chetan Urf Ishant <ishunara25@gmail.com>
2026-05-30 14:13:32 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -276,3 +276,44 @@ int main()
         cout<<endl;
     }
 }
+
+//14
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int n;
+    cout<<"Enter the number of lines: ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<alphabet[j];
+        }
+        cout<<endl;
+    }
+}
+
+//15
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int n;
+    cout<<"Enter the number of lines: ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<alphabet[j];
+        }
+        cout<<endl;
+    }
+}
+

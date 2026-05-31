af1d154f0edbfe5a11faac83177fcfad3e2481b8
Chetan <ishunara25@gmail.com>
2026-05-31 15:33:41 +0530

    Update all_patterns.cpp

--- 01_patterns/all_patterns.cpp
+++ 01_patterns/all_patterns.cpp
@@ -1,5 +1,17 @@
 //                    # This file includes all the pattern codes as per strivers A to Z DSA sheet
 
+//1
+#include<iostream>
+using namespace std;
+int main(){
+	int n=0;
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
 
 //2
 #include<iostream>
